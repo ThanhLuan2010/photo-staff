17086d2b9b772971f0f7d64f57645829795cb804
Lê Đặng Phương Uyên <renyuiko.hamyana@gmail.com>
2024-06-04 16:08:37 +0700

    create table coupon data

--- src/component/nav/NavBar.tsx
+++ src/component/nav/NavBar.tsx
@@ -214,6 +214,7 @@ export default function NavBar() {
                 dispatch(setIsLogin(false));
                 dispatch(setToken(""));
                 dispatch(setUserInfo({}));
+                navigate("/");
               }}
               className="flex items-center self-center text-xl text-white"
             >
